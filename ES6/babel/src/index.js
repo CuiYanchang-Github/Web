d30d7434d5b393d746896d0103a5f5d052c9742b
CuiYanchang-Github <cyc982109@163.com>
2020-02-07 12:02:42 +0800

    web

---
+++ ES6/babel/src/index.js
@@ -0,0 +1,11 @@
+let a = "hello";
+let a = "world";
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    showInfo(){
+        console.log(this.name);
+    }
+}
