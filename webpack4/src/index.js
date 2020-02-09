9b12f95f87def603832ee3a69cffe8394856d668
CuiYanchang-Github <cyc982109@163.com>
2020-02-09 14:18:42 +0800

    web

--- webpack4/src/index.js
+++ webpack4/src/index.js
@@ -1,1 +1,5 @@
-let str = "hello world!"
+window.onload = function(){
+    let str = "hello world!";
+    let oDiv1 = document.getElementById("div1");
+    oDiv1.innerHTML = str ;
+}
--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -1,10 +1,17 @@
-const path = require("path")
+const path = require("path");
 module.exports={
+    mode:'development',
     entry:{
         'index':'./src/index.js',
     },
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js'
+    },
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'127.0.0.1',
+        compress:true,
+        port:'8081',
     }
 }
