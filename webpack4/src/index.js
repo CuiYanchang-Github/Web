82926453f799c19e8584f9dfd5397cf92f559179
CuiYanchang-Github <cyc982109@163.com>
2020-02-10 11:45:45 +0800

    web

--- webpack4/src/index.js
+++ webpack4/src/index.js
@@ -1,5 +1,11 @@
+import css from './css/main.css';
+import scaa from './style/test.scss';
+
 window.onload = function(){
     let str = "hello world!";
     let oDiv1 = document.getElementById("div1");
     oDiv1.innerHTML = str ;
+    let num = 100;
+    console.log(num);
+
 }
--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -1,17 +1,68 @@
 const path = require("path");
-module.exports={
-    mode:'development',
-    entry:{
-        'index':'./src/index.js',
-    },
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'[name].js'
-    },
-    devServer:{
-        contentBase:path.resolve(__dirname,'dist'),
-        host:'127.0.0.1',
-        compress:true,
-        port:'8081',
-    }
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+  mode: "development", //设置打包模式
+  entry: {
+    'index': './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: 'http://127.0.0.1:8081/'
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use:[{
+            loader:'css-loader',
+            options:{importLoaders:1}
+          },'postcss-loader']
+        })
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 500,
+            outputPath: 'images/'
+          }
+        }]
+      },
+      {
+        test: /\.(htm|html)$/i,
+        loader: 'html-withimg-loader'
+      },
+      {
+        test: /\.scss/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      hash: true,
+      minify: {
+        removeAttributeQuotes: true
+      }
+    }),
+    new ExtractTextPlugin("css/main.css")
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    host: "127.0.0.1",
+    compress: true,
+    port: '8081',
+    open: true,
+    hot: true
+  }
 }
