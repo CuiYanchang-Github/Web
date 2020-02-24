9494f47802c0dad390d6c0fe7d63fef2ccfc008e
CuiYanchang-Github <cyc982109@163.com>
2020-02-24 20:43:03 +0800

    web

--- Reactcli/myapp/src/views/Page5.js
+++ Reactcli/myapp/src/views/Page5.js
@@ -9,12 +9,12 @@ import {
 const Com1 = () => <h1>Com1</h1>
 const Com2 = () => <h1>Com2</h1>
 
+//自定义链接其实他就是一个函数组件
 const MenuLink = ({to,label}) =>{
   return(
     <Route
       path={to}
       children={({match})=>{
-        console.log(match)
         return(
           <div>
             {match?'>':""}
@@ -28,15 +28,17 @@ const MenuLink = ({to,label}) =>{
 
 export default class Page5 extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
         <h1>Page5</h1>
         <Router>
-          <MenuLink to='/page5/com1' label="com1" />|
-          <MenuLink to='/page5/com2' label="com2" />
+          <MenuLink to={this.props.match.path+'/com1'} label="com1" />|
+          <MenuLink to={this.props.match.path+'/com2'} label="com2" />
+          {/* <Route path="/page5/test" children={({match})=>(<Link to="/page5/test">test</Link>)} /> */}
           <Switch>
-            <Route path="/page5/com1" component={Com1}/>
-            <Route path="/page5/com2" component={Com2}/>
+            <Route path={this.props.match.path+'/com1'} component={Com1}/>
+            <Route path={this.props.match.path+'/com2'} component={Com2}/>
           </Switch>
         </Router>
       </div>
